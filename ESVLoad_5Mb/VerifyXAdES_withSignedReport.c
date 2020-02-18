c9782a61e39c3b0d639c09ee2cb048c4bf48c08b
Троицкий Дмитрий Александрович <d.troickiy@voskhod.ru>
2020-02-18 16:23:46 +0300

    Добавлены скрипты для 5 МБ

---
+++ ESVLoad_5Mb/VerifyXAdES_withSignedReport.c
@@ -0,0 +1,31 @@
+VerifyXAdES_withSignedReport()
+{
+	web_cache_cleanup();
+	web_cleanup_auto_headers();
+	web_cleanup_cookies();
+
+	lr_output_message(	lr_eval_string("{dataPath}/{VerifyXAdES_withSignedReport_file}"));
+	lr_read_file(lr_eval_string("{dataPath}/{VerifyXAdES_withSignedReport_file}"), "request", 0);
+
+	lr_output_message("Use file: {VerifyXAdES_withSignedReport_file}");
+
+	lr_start_transaction("VerifyXAdES_withSignedReport");
+	soap_request("StepName=VerifyXAdES_withSignedReport",
+		"URL={URL}",
+		"SOAPEnvelope="
+			"{request}",
+		"ExpectedResponse=AnySoap",
+		"ResponseParam=result",
+		LAST);
+
+	//lr_output_message("log/result: %s", lr_eval_string("{result}"));
+	if(strstr( lr_eval_string("{result}"), lr_eval_string("{VerifyXAdES_withSignedReport_response}"))==NULL)
+	{
+        lr_end_transaction("VerifyXAdES_withSignedReport", LR_FAIL);
+		lr_output_message( lr_eval_string("error: expected contains {VerifyXAdES_withSignedReport_response}, but got {result}"));
+		return -1;
+	}
+
+    lr_end_transaction("VerifyXAdES_withSignedReport", LR_PASS);
+	return 0;
+}
