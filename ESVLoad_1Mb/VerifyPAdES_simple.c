73be1c2dfb1e1da650e53a8ae6aee9c998aa3e64
Первушин Алексей Леонидович <a.pervushin@voskhod.ru>
2020-02-18 12:46:08 +0300

    Добавлен код в ESVLoad_1Mb

---
+++ ESVLoad_1Mb/VerifyPAdES_simple.c
@@ -0,0 +1,31 @@
+VerifyPAdES_simple()
+{
+	web_cache_cleanup();
+	web_cleanup_auto_headers();
+	web_cleanup_cookies();
+
+	lr_output_message(	lr_eval_string("{dataPath}/{VerifyPAdES_simple_file}"));
+	lr_read_file(lr_eval_string("{dataPath}/{VerifyPAdES_simple_file}"), "request", 0);
+
+	lr_output_message("Use file: {VerifyPAdES_simple_file}");
+
+	lr_start_transaction("VerifyPAdES_simple");
+	soap_request("StepName=VerifyPAdES_simple",
+		"URL={URL}",
+		"SOAPEnvelope="
+			"{request}",
+		"ExpectedResponse=AnySoap",
+		"ResponseParam=result",
+		LAST);
+
+	//lr_output_message("log/result: %s", lr_eval_string("{result}"));
+	if(strstr( lr_eval_string("{result}"), lr_eval_string("{VerifyPAdES_simple_response}"))==NULL)
+	{
+        lr_end_transaction("VerifyPAdES_simple", LR_FAIL);
+		lr_output_message( lr_eval_string("error: expected contains VerifyPAdES_simple_response}, but got {result}"));
+		return -1;
+	}
+
+    lr_end_transaction("VerifyPAdES_simple", LR_PASS);
+	return 0;
+}
